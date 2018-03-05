9292e5d09e294206cdb1336cc893b52013eab2d5
Judzhin Miles <info@msbios.com>
2018-03-05 18:02:59 +0200

    Lazy Composer Commiter

--- src/Repository/NewsRepository.php
+++ src/Repository/NewsRepository.php
@@ -16,4 +16,16 @@ use MSBios\Resource\Doctrine\Repository\PaginatorRepositoryTrait;
 class NewsRepository extends EntityRepository
 {
     use PaginatorRepositoryTrait;
+
+    /**
+     * @param int $page
+     * @param int $limit
+     * @return mixed
+     */
+    public function getPaginator($page = 1, $limit = 20)
+    {
+        return $this->buildPaginator($this->createQueryBuilder('n'))
+            ->setDefaultItemCountPerPage($limit)
+            ->setCurrentPageNumber($page);
+    }
 }
