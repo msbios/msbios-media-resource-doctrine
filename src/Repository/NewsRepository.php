cf4d6a9343ad90ab3f19d47ba71219d814029902
Judzhin Miles <info@msbios.com>
2018-03-06 11:29:49 +0200

    Lazy Composer Commiter

--- src/Repository/NewsRepository.php
+++ src/Repository/NewsRepository.php
@@ -6,9 +6,12 @@
 
 namespace MSBios\Media\Resource\Doctrine\Repository;
 
+use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use MSBios\Resource\Doctrine\Repository\PaginatorRepositoryTrait;
+use Zend\InputFilter\Factory;
+use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterAwareInterface;
 use Zend\InputFilter\InputFilterInterface;
 use Zend\Paginator\Paginator;
@@ -40,5 +43,63 @@ class NewsRepository extends EntityRepository
         $paginator->setDefaultItemCountPerPage($limit);
         $paginator->setCurrentPageNumber($page);
         return $paginator;
+    }
+
+    /**
+     * @param array $params
+     * @return Paginator
+     */
+    public function getPaginatorFromQuery(array $params)
+    {
+        /** @var InputFilterInterface $factory */
+        $inputFilter = (new Factory)->createInputFilter([
+            [
+                'name' => 'title',
+                'required' => false,
+            ], [
+                'type' => InputFilter::class,
+                'name' => 'postdate',
+                [
+                    'name' => 'from',
+                    'required' => false,
+                ], [
+                    'name' => 'to',
+                    'required' => false,
+                ]
+            ]
+        ])->setData($params);
+
+        return $this->getPaginatorWith(function (QueryBuilder $qb) use ($inputFilter) {
+
+            if ($inputFilter->isValid()) {
+
+                /** @var array $values */
+                $values = $inputFilter->getValues();
+
+                if (!empty($values['title'])) {
+                    $qb->where('n.title LIKE :title')
+                        ->setParameter('title', "{$values['title']}%");
+                }
+
+                if (!empty($values['postdate']['from']) && !empty($values['postdate']['to'])) {
+
+                    $qb->andWhere('n.postdate BETWEEN :from AND :to')
+                        ->setParameter('from', new \DateTime($values['postdate']['from']), Type::DATETIME)
+                        ->setParameter('to', new \DateTime($values['postdate']['to']), Type::DATETIME);
+
+                } elseif (!empty($values['postdate']['from'])) {
+
+                    $qb->andWhere('n.postdate > :from')
+                        ->setParameter('from', new \DateTime($values['postdate']['from']), Type::DATETIME);
+
+                } elseif (!empty($values['postdate']['to'])) {
+
+                    $qb->andWhere('n.postdate < :to')
+                        ->setParameter('to', new \DateTime($values['postdate']['to']), Type::DATETIME);
+
+                }
+
+            }
+        });
     }
 }
